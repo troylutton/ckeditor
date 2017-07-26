1b123ff7ccb1788a797a31f0a0e033cf724db3c3
HERMES\Troy <troy.lutton@gmail.com>
2017-07-26 10:31:14 +1000

    Untested but first cut of localization and bower integration

---
+++ placeholder_select/locale/es.js
@@ -0,0 +1,12 @@
+(function () { 
+    
+    'use strict';
+
+    return CKEDITOR.plugins.placeholder_select.defineLocale('es', {
+        labels: {
+            dropdown_label: 'Insertar símbolo',
+            dropdown_title: 'Insertar símbolo',
+            dropdown_voiceLabel: 'Insertar símbolo'
+        }
+    });
+});
--- placeholder_select/plugin.js
+++ placeholder_select/plugin.js
@@ -2,14 +2,10 @@
  * A plugin to enable placeholder tokens to be inserted into the CKEditor message. Use on its own or with teh placeholder plugin. 
  * The default format is compatible with the placeholders syntex
  *
- * @version 0.1 
+ * @version 0.2
  * @Author Troy Lutton
  * @license MIT 
- * 
- * This is a pure modification for the placeholders plugin. All credit goes to Stuart Sillitoe for creating the original (stuartsillitoe.co.uk)
- *
  */
-
 CKEDITOR.plugins.add('placeholder_select',
 {
 	requires : ['richcombo'],
@@ -20,26 +16,30 @@ CKEDITOR.plugins.add('placeholder_select',
 		
 		// init the default config - empty placeholders
 		var defaultConfig = {
+			locale: 'en',
 			format: '[[%placeholder%]]',
-			placeholders : []
+			placeholders : []			
 		};
 
 		// merge defaults with the passed in items		
-		var config = CKEDITOR.tools.extend(defaultConfig, editor.config.placeholder_select || {}, true);
+		var config = CKEDITOR.tools.extend(defaultConfig, editor.config.placeholder_select || {}, true);		
+
+		// get the localized labels
+		var labels = CKEDITOR.plugins.placeholder_select.getLabels(config.locale);
 
 		// run through an create the set of items to use
 		for (var i = 0; i < config.placeholders.length; i++) {
 			// get our potentially custom placeholder format
 			var placeholder = config.format.replace('%placeholder%', config.placeholders[i]);			
 			placeholders.push([placeholder, config.placeholders[i], config.placeholders[i]]);
-		}
+		}		
 
 		// add the menu to the editor
 		editor.ui.addRichCombo('placeholder_select',
 		{
-			label: 		'Insert placeholder',
-			title: 		'Insert placeholder',
-			voiceLabel: 'Insert placeholder',
+			label: 		labels.dropdown_label,
+			title: 		labels.dropdown_title,
+			voiceLabel: labels.dropdown_voiceLabel,
 			className: 	'cke_format',
 			multiSelect:false,
 			panel:
@@ -50,9 +50,10 @@ CKEDITOR.plugins.add('placeholder_select',
 
 			init: function()
 			{
-				this.startGroup( "Insert placeholder" );
+				this.startGroup( labels.dropdown_title );
 				for (var i in placeholders)
 				{
+					// format
 					this.add(placeholders[i][0], placeholders[i][1], placeholders[i][2]);
 				}
 			},
@@ -67,3 +68,51 @@ CKEDITOR.plugins.add('placeholder_select',
 		});
 	}
 });
+
+// Methods and variables related to the plugin but exposed to the public.
+CKEDITOR.plugins.placeholder_select = {	
+
+	default: 'en',
+
+	// setup the default
+	locales: {
+		en: {
+			labels: {
+				dropdown_label: 'Insert placeholder',
+				dropdown_title: 'Insert placeholder',
+				dropdown_voiceLabel: 'Insert placeholder'
+			}			
+		}
+	},
+
+	getLabels: function(locale) {
+		
+		if (!locales.hasOwnProperty(locale)) {
+			console.warn('The translation does not exist for ckeditor plugin placeholder_select.  Defaulting to ' + CKEDITOR.plugins.placeholder_select.default);
+			return locales[CKEDITOR.plugins.placeholder_select.default];
+		}
+
+		return locales[locale];
+	},
+
+	// adds a locale to the available ones
+	// must conform to a format eg: { 'en': { labels: { etc } }}
+	defineLocale: function(locale, config) {
+
+		// store in the locale if it doesn't exist
+		if (!locales.hasOwnProperty(locale)) {
+			locales[locale] = config;
+		}
+
+		return locales[locale];
+	},
+
+	// wrapper for ckeditors loader
+	loadLocale: function(url) {
+
+		// load in the locale file to provide translatable labelling
+		CKEDITOR.scriptLoaderView.load(url, function(scope, success) {			
+
+		});
+	}
+};
