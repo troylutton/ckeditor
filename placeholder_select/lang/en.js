eccef27f622240f8d227f4c0b4ce0461fa54484b
Theofilos Intzoglou <int.teo@gmail.com>
2020-12-10 15:53:14 +0200

    Add localization support and greek translation

---
+++ placeholder_select/lang/en.js
@@ -0,0 +1,5 @@
+CKEDITOR.plugins.setLang('placeholder_select', 'en', {
+    dropdown_label: 'Insert placeholder',
+    dropdown_title: 'Insert placeholder',
+    dropdown_voiceLabel: 'Insert placeholder'
+});
