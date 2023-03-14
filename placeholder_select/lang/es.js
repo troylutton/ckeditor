d1cac585a3b2d8b63a97ae636b78dfabb35d6395
Stefan Kientzler <s.kientzler@online.de>
2023-03-14 19:42:47 +0100

    added languages: "es", "eu", "fr", "it"

---
+++ placeholder_select/lang/es.js
@@ -0,0 +1,6 @@
+﻿CKEDITOR.plugins.setLang("placeholder_select_ext", "es", {
+  dropdown_label: "Marcador de Posición",
+  dropdown_group: "Marcador de Posición",
+  dropdown_title: "Marcador de Posición",
+  dropdown_voiceLabel: "Marcador de Posición",
+});
---
+++ placeholder_select/lang/eu.js
@@ -0,0 +1,6 @@
+﻿CKEDITOR.plugins.setLang('placeholder_select', 'eu', {
+    dropdown_label: 'Leku-marka',
+    dropdown_group: 'Leku-marka',
+    dropdown_title: 'Leku-marka',
+    dropdown_voiceLabel: 'Leku-marka'
+});
